d13e9664ab6cfdfd53c9e45c26fe9d3fb1c624f1
Jarvishappy <jarvisHappyHacking@gmail.com>
2015-08-01 16:58:37 +0800

    init repo

---
+++ 410kern/misc/kernel.mk
@@ -0,0 +1,10 @@
+410K_MISC_OBJS := \
+					gccisms.o \
+					nonogram_db.o \
+
+410K_MISC_OBJS := $(410K_MISC_OBJS:%=$(410KDIR)/misc/%)
+
+ALL_410KOBJS += $(410K_MISC_OBJS)
+410KCLEANS += $(410KDIR)/libmisc.a
+
+$(410KDIR)/libmisc.a: $(410K_MISC_OBJS)
